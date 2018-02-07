1e310ec0cdde0e1b75e6b4db64f4c1ff2c32c435
Chupa.Chups <gusinacenis88@gmail.com>
2018-02-07 17:21:38 +0100

    populated propertie in Currency model

--- app/models/Currency.java
+++ app/models/Currency.java
@@ -1,5 +1,20 @@
 package models;
 
 public class Currency {
+    public String id;
+    public String name;
+    public String symbol;
+    public Integer rank;
+    public Double price_usd;
+    public Double price_btc;
+    public Double market_cap_usd;
+    public Double available_supply;
+    public Double total_supply;
+    public Double max_supply;
+    public Double percent_change_1h;
+    public Double percent_change_24h;
+    public Double percent_change_7d;
+    public Integer last_updated;
+
 
 }
