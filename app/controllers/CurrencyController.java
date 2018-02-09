329a12e31e49af1474436c1b115f48cdd66442aa
Enis Gusinac <gusinacenis88@gmail.com>
2018-02-09 22:15:50 +0100

    Given the values to the Object properties and display the values in the view

--- app/controllers/CurrencyController.java
+++ app/controllers/CurrencyController.java
@@ -1,6 +1,5 @@
 package controllers;
 
-import models.Currency;
 import play.mvc.Controller;
 import play.mvc.Result;
 import repositories.CurrencyRepository;
--- app/repositories/CurrencyRepository.java
+++ app/repositories/CurrencyRepository.java
@@ -1,7 +1,6 @@
 package repositories;
 
 import models.Currency;
-
 import java.util.HashSet;
 import java.util.Set;
 
@@ -9,16 +8,21 @@ public class CurrencyRepository {
 
     public static Set<Currency> allCurrencies() {
         Set<Currency> currencies = new HashSet<>();
+
+        currencies.add(new Currency("bitcoin", "Bitcoin",  "BTC", 1, 573.137, 1.0,
+                72855700.0, 9080883500.0,15844176.0,15844176.0,0.0,
+                0.04,-0.3,-0.57,1472762067));
+
         currencies.add(new Currency(
-                "ETH", "Ethereum", "", 1, 1.1, 1.1,
-                1.1, 1.1,1.1,1.1,1.1,
-                1.1,1.1,1.1,1));
-        currencies.add(new Currency("BTC", "Bitcoin",  "", 1, 1.1, 1.1,
-                1.1, 1.1,1.1,1.1,1.1,
-                1.1,1.1,1.1,1));));
-        currencies.add(new Currency("LTC", "Litecoin",  "", 1, 1.1, 1.1,
-                1.1, 1.1,1.1,1.1,1.1,
-                1.1,1.1,1.1,1));
+                "ethereum", "Ethereum", "ETH", 2, 12.1844, 0.021262,
+                24085900.0, 1018098455.0,83557537.0,83557537.0,0.0,
+                -0.58,6.34,8.59,1472762062));
+
+        currencies.add(new Currency("ripple", "Ripple",  "XRP", 3, 0.916231, 0.00010707,
+                1710090000.0, 35741452836.0,39009215838.0,99992725510.0,100000000000.0,
+                -0.94,19.82,6.63,1518210241));
+
+
         return currencies;
     }
 
